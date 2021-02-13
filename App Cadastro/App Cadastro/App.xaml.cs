55a56e1b7b54be1ca9343ba8eb1a935325b34d02
rafaelvsmello <rmellov@gmail.com>
2021-02-13 11:58:35 -0300

    Add project files.

---
+++ App Cadastro/App Cadastro/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App_Cadastro.View;
+
+namespace App_Cadastro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Home());
+        }
+        
+    }
+}
---
+++ App Cadastro/App Cadastro/View/Home.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Cadastro.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCadastrar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cadastro());
+        }
+
+        private async void btnAlterar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Alterar());
+        }
+
+        private async void btnExluir_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Excluir());
+        }
+
+        private async void btnListar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Listar());
+        }
+    }
+}
