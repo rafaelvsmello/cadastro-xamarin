8e4422d61b259353c82317befef9f77e2138323b
rafaelvsmello <rmellov@gmail.com>
2021-02-13 15:46:54 -0300

    Inclusão da classe de dados e finalização da tela de cadastros

---
+++ App Cadastro/App Cadastro/Models/Dados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_Cadastro.Models
+{
+    public class Dados
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Status { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
--- App Cadastro/App Cadastro/View/Cadastro.xaml.cs
+++ App Cadastro/App Cadastro/View/Cadastro.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using App_Cadastro.Models;
+using LiteDB;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,14 +10,68 @@ namespace App_Cadastro.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Cadastro : ContentPage
     {
+        public string GetFilePath(string file)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, file);
+        }
+
+        void Cadastrar()
+        {
+            try
+            {
+                using (var db = new LiteDatabase(GetFilePath("Data.db")))
+                {
+                    var col = db.GetCollection<Dados>("dados");
+
+                    var dados = new Dados
+                    {
+                        Descricao = txtDescricao.Text,
+                        Data = dtpData.Date,
+                        Status = pckStatus.SelectedItem.ToString()
+                    };
+
+                    col.Insert(dados);
+                    DisplayAlert("Atenção", "Cadastro realizado com sucesso", "OK");
+                    txtDescricao.Text = string.Empty;
+                    pckStatus.SelectedIndex = -1;
+                    dtpData.Date = DateTime.Now;
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ocorreu um erro", ex.Message, "OK");
+            }
+            
+        }
+
         public Cadastro()
         {
             InitializeComponent();
+
+            pckStatus.Items.Add("Pendente");
+            pckStatus.Items.Add("Finalizado");
         }        
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        private void btnCadastrar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDescricao.Text))
+            {
+                DisplayAlert("Atenção", "Informe a descrição", "OK");
+            }
+            else if (pckStatus.SelectedIndex == -1)
+            {
+                DisplayAlert("Atenção", "Informe um status", "OK");
+            }
+            else
+            {
+                Cadastrar();
+            }
         }
     }
 }
